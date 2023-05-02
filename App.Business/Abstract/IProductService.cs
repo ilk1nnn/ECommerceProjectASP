af26aa92a8dbb8923fa226681079556a6fe7549d
ilkin <ilkin@MY_ASUS>
2023-05-02 11:52:33 +0400

    Add project files.

---
+++ App.Business/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using App.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategory(int categoryId);
+        void Add(Product product);  
+        void Update(Product product);
+        void Delete(int id);
+        Product GetById(int id);
+
+    }
+}
---
+++ ECommerce.WebUI/Models/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.WebUI.Models
+{
+    public class ProductViewModel
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
